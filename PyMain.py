2a9d3f1bd625c393be97ba57e2dfc3d95087bebb
SATYAVEER <39643146+veermit@users.noreply.github.com>
2020-01-18 14:32:49 +0530

    Initial Update
    main() executable code related operations performed here.

---
+++ PyMain.py
@@ -0,0 +1,46 @@
+# Python official website
+
+#!/usr/bin/env python3
+
+# print ((2 * ' ') + (2 * '*') +  (10 * ' ') + (2 * '*'))
+# print ((1 * ' ') + (3 * '*') +  (10 * '*') + (3 * '*'))
+# print ((2 * '*') + (3 * '*') +  (10 * '*') + (3 * '*'))
+# print ((1 * ' ') + (3 * '*') +  (10 * '*') + (3 * '*'))
+# print ((2 * ' ') + (2 * '*') +  (10 * ' ') + (2 * '*'))
+
+#   **          **
+#  ****************
+# ******************
+#  ****************
+#   **          **
+
+# RetVal = (175/3)
+# print (RetVal)		# 58.333333333333336
+# print (175//3)		# 58
+# print (175%3)		# 1
+
+# Define function (Definitions will be in spaced with indent)
+def main():
+	print ("Inside main")
+
+if __name__ == "__main__":
+	main()
+
+# print (__name__)	# prints __main__
+
+# Val = 10					# This defines Val as global variable
+
+# def Global():
+# 	global Val 				# This defines Val as gloabl using Global keyword
+# 	print (Val)
+# 	Val = "Gloabl"
+
+# def Local():
+# 	Val = 20
+# 	print (Val)
+
+# Global()
+# Local()
+
+# del Val
+# print (Val)    
