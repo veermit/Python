2c8d84373f7b7c7535709af15e3572b60ed77a0f
SATYAVEER <39643146+veermit@users.noreply.github.com>
2020-01-18 14:33:30 +0530

    Intial Update
    String related operations performed

---
+++ PyString.py
@@ -0,0 +1,14 @@
+# Python official website
+def main():
+	StringTest()
+
+def StringTest():
+	Var_1 = "Satyveer"
+	Var_2 = "Verma"
+
+	print ("Var_1[0]:", 	Var_1[0])
+	print ("Var_2[1:3]:", 	Var_2[1:3])
+
+# If main exists then only main() will execute
+if __name__ == "__main__":
+	main()   
